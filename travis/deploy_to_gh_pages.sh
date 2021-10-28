d4b30c9a8a787c3f241af1c88693e691c0e3f09a
ndp-opendap <ndp@opendap.org>
2021-10-28 03:56:07 -0700

    Tweaking deployment for index.html

--- travis/deploy_to_gh_pages.sh
+++ travis/deploy_to_gh_pages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+VERSION=`cat VERSION`"-${TRAVIS_BUILD_NUMBER}"
+echo "VERSION: ${VERSION}"
 
 index_html=$(
 cat <<EOF
@@ -19,18 +21,18 @@ cd output
 cp -R ../images images
 
 # GitHub Pages requires an functional index.html file.
-# I tried using an html redirect in the index.html
-# But that was always a self redirect  never to the
-# target specified in the url attrbute file but that alway redirected to the same page.
-
+# Here we add an index.html that redirects to Master_Hyrax_Guide.html
 echo "${index_html}" | tee index.html
-# cp Master_Hyrax_Guide.html index.html
-git init
+
+# Now we set up the git repo
 git config user.name "The-Robot-Travis"
 git config user.email "npotter@opendap.org"
+
+# Add and commit the content
 git add .
 git status
 git commit -m "Deploying update to GitHub Pages"
 git status
+# Push to GitHub Pages
 git push --force -v "https://$GIT_UID:$GIT_TOKEN@github.com/OPENDAP/hyrax_guide" master:gh-pages; echo $?
 git status
