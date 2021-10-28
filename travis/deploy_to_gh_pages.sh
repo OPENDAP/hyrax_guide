7482f7818b7d1319610c8d67b4a5089d33583c65
ndp-opendap <ndp@opendap.org>
2021-10-28 03:12:28 -0700

    Fixing deployment

---
+++ travis/deploy_to_gh_pages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+index_html=$(
+cat <<EOF
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Hyrax Guide</title>
+      <meta http-equiv="refresh" content ="0" url="./Master_Hyrax_Guide.html" />
+   </head>
+   <body>
+      <p>Redirecting to another URL</p>
+   </body>
+</html>
+EOF
+)
+
+
+cd output
+cp -R ../images images
+echo "${index_html}" > index.html
+git init
+git config user.name "The-Robot-Travis"
+git config user.email "npotter@opendap.org"
+git add .
+git status
+git commit -m "Deploying update to GitHub Pages"
+git status
+git push --force -v "https://$GIT_UID:$GIT_TOKEN@github.com/OPENDAP/hyrax_guide" master:gh-pages; echo $?
+git status
